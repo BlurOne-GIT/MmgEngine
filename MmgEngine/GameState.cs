08503bcbc65989c697173f69e4e41bd99a0c81f8
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-03-22 21:04:04 -0300

    feat: old engine

---
+++ MmgEngine/GameState.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace MmgEngine;
+
+public abstract class GameState : DrawableGameComponent
+{
+    public GameState(Game game) : base(game)
+    {
+        Input.KeyDown += HandleInput;
+        Input.ButtonDown += HandleInput;
+    }
+    public new virtual void Dispose()
+    {
+        Input.KeyDown -= HandleInput;
+        Input.ButtonDown -= HandleInput;
+        foreach (GameComponent gameObject in _components)
+            gameObject.Dispose();
+
+        base.Dispose();
+    }
+    protected readonly GameComponentCollection _components = new GameComponentCollection();
+    public virtual void HandleInput(object s, ButtonEventArgs e) {}
+    public virtual void HandleInput(object s, InputKeyEventArgs e) {}
+    public event EventHandler<GameState> OnStateSwitched;
+    public new abstract void LoadContent();
+    public new abstract void UnloadContent();
+    protected void SwitchState(GameState gameState)
+    {
+        OnStateSwitched?.Invoke(this, gameState);
+    }
+    public override void Update(GameTime gameTime)
+    {
+        foreach (GameComponent gameObject in _components.OrderBy(a => (a as GameComponent).UpdateOrder))
+            if (gameObject.Enabled) gameObject.Update(gameTime);
+    }
+    public override void Draw(GameTime gameTime)
+    {
+        var c = _components.Where(a => a is DrawableGameComponent && (a as DrawableGameComponent).Visible).OrderBy(a => (a as DrawableGameComponent).DrawOrder);
+        foreach (DrawableGameComponent gameObject in c)
+            gameObject.Draw(gameTime);
+    }
+ }
---
+++ MmgEngine/Input.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MmgEngine;
+
+public class ButtonEventArgs : EventArgs
+{
+    public ButtonEventArgs(string button, Point position) { Button = button; Position = position; }
+    public string Button { get; }
+    public Point Position { get; }
+}
+
+public static class Input
+{
+    #region Events
+    public static event EventHandler<ButtonEventArgs> ButtonDown;
+    public static event EventHandler<ButtonEventArgs> ButtonUp;
+
+    public static event EventHandler<InputKeyEventArgs> KeyDown;
+    public static event EventHandler<InputKeyEventArgs> KeyUp;
+    #endregion
+
+    #region Fields
+    private static bool _leftButton;
+    private static bool _middleButton;
+    private static bool _rightButton;
+    private static bool _xButton1;
+    private static bool _xButton2;
+    private static Point _position;
+    private static bool[] _keys = new bool[255];
+    #endregion
+
+    #region Properties
+    public static bool ShowCursor { get; set; }
+    public static Point MousePoint { get; set; }
+    private static bool LeftButton { set => CheckInput(ref _leftButton, value); }
+    private static bool MiddleButton { set => CheckInput(ref _middleButton, value); }
+    private static bool RightButton { set => CheckInput(ref _rightButton, value); }
+    private static bool XButton1 { set => CheckInput(ref _xButton1, value); }
+    private static bool XButton2 { set => CheckInput(ref _xButton2, value); }
+    #endregion
+
+    #region Methods
+    public static void UpdateInputs(MouseState mouseState)
+    {
+        LeftButton = Convert.ToBoolean(mouseState.LeftButton);
+        MiddleButton = Convert.ToBoolean(mouseState.MiddleButton);
+        RightButton = Convert.ToBoolean(mouseState.RightButton);
+        XButton1 = Convert.ToBoolean(mouseState.XButton1);
+        XButton2 = Convert.ToBoolean(mouseState.XButton2);
+        _position = mouseState.Position;
+    }
+    public static void UpdateInputs(KeyboardState keyboardState)
+    {
+        for (int i = 0; i < 255; i++)
+        {
+            CheckInput(i, keyboardState.IsKeyDown((Keys)i));
+        }
+    }
+
+    private static void CheckInput(ref bool button, bool value, [CallerMemberName] string name = null)
+    {
+        if (button == value)
+            return;
+        
+        button = value;
+        if (!TheLightbulb.Statics.WindowFocused || MousePoint.X < 0 || MousePoint.Y < 0 || MousePoint.X > 800 || MousePoint.Y > 800)
+            return;
+        if (value)
+            ButtonDown?.Invoke(null, new ButtonEventArgs(name, _position));
+        else
+            ButtonUp?.Invoke(null, new ButtonEventArgs(name, _position));
+    }
+    private static void CheckInput(int i, bool value)
+    {
+        if (_keys[i] == value)
+            return;
+
+        _keys[i] = value;
+        if (!TheLightbulb.Statics.WindowFocused)
+            return;
+        if (value)
+            KeyDown?.Invoke(null, new InputKeyEventArgs((Keys)i));
+        else
+            KeyUp?.Invoke(null, new InputKeyEventArgs((Keys)i));
+    }
+    #endregion
+}
