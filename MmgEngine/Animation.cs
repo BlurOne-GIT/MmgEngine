78362c5335a3c578c294d36f4ac6d4dbe246baad
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-03-25 16:57:33 -0300

    refactor: Animation

--- MmgEngine/Animation.cs
+++ MmgEngine/Animation.cs
@@ -6,8 +6,8 @@ namespace MmgEngine;
 public class Animation<T>
 {
     #region Fields
-    private readonly T[] _frames;
-    private int _pos;
+    public T[] Frames { get; }
+    public int Position { get; set; }
     private int _frameDelay;
     private readonly bool _isLooped;
     private readonly int _frameDuration;
@@ -16,53 +16,42 @@ public class Animation<T>
 
     public Animation(T[] frames, bool looped, int frameDuration = 1)
     {
-        _frames = frames;
+        Frames = frames;
         _isLooped = looped;
-        _pos = _frames.Length - 1;
+        Position = Frames.Length - 1;
         _frameDuration = frameDuration - 1;
         _frameDelay = _frameDuration;
     }
 
-    public void Start() => _pos = 0;
+    public void Start() => Position = 0;
 
     public T NextFrame()
     {
         if (Paused)
             return CurrentFrame();
 
-        if (_pos > _frames.Length - 1)
-        {
+        if (Position >= Frames.Length)
             if (_isLooped)
-                _pos = 0;
+                Position = 0;
             else
-                return _frames[_pos-1];
-        }
+                return Frames[Position-1];
 
-        if (_frameDelay-- > 0)
-            return _frames[_pos];
+        if (--_frameDelay > 0)
+            return Frames[Position];
 
         _frameDelay = _frameDuration;
 
-        return _frames[Paused ? _pos : _pos++];
+        return Frames[Paused ? Position : Position++];
     }
 
-    public T CurrentFrame()
-    {
-        return _pos < _frames.Length ? _frames[_pos] : _frames[_pos-1];
-    }
-
-    public T[] GetFrames() => _frames;
+    public T CurrentFrame() => Position < Frames.Length ? Frames[Position] : Frames[Position-1];
 
     public static Animation<Rectangle> TextureAnimation(Point frameSize, Point bounds, bool looped, int frameDuration)
     {
         var frames = new List<Rectangle>();
         for (int y = 0; y < bounds.Y; y += frameSize.Y)
-        {
             for (int x = 0; x < bounds.X; x += frameSize.X)
-            {
                 frames.Add(new Rectangle(x, y, frameSize.X, frameSize.Y));
-            }
-        }
 
         return new Animation<Rectangle>(frames.ToArray(), looped, frameDuration);
     }
