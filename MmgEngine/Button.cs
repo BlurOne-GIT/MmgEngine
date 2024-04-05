07f1d2ab7ab3c9e4fe1669202d869725a0f4b459
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-04-05 18:53:58 -0300

    feat(ActionBoxes): Viewport

--- MmgEngine/Button.cs
+++ MmgEngine/Button.cs
@@ -16,46 +16,62 @@ public class Button : DrawableGameComponent
     #region Properties
     public SimpleImage Image { get; private set; }
     public HoverDetector HoverDetector { get; private set; }
-    public Rectangle ActionBox { get; private set; }
+    public Vector2 Position
+    {
+        get => new(_actionBox.X, _actionBox.Y);
+        set
+        {
+            _actionBox.Location = (value - _actionBox.Size.ToVector2() * EngineStatics.Aligner(Alignment.TopLeft)).ToPoint();
+            if (HoverDetector != null) HoverDetector.Position = value;
+        }
+    }
+
+    public Vector2 Size
+    {
+        get => new(_actionBox.Width, _actionBox.Height);
+        set
+        {
+            _actionBox.Size = value.ToPoint();
+            _actionBox.Location -= (_actionBox.Size.ToVector2() * EngineStatics.Aligner(_alignment)).ToPoint();
+            if (HoverDetector != null) HoverDetector.Size = value;
+        }
+    }
+    #endregion
+
+    #region Fields
+    private Rectangle _actionBox;
+    private readonly Alignment _alignment;
     #endregion
 
     //Constructor
-    public Button(Game game, Rectangle actionBox, SimpleImage texture = null, Alignment anchor = Alignment.TopLeft, bool enabled = true, bool hasHover = false) : base(game)
+    public Button(Game game, Rectangle actionBox, SimpleImage texture = null, Alignment alignment = Alignment.TopLeft, bool hasHover = false) : base(game)
     {
-        actionBox.Location -= (actionBox.Size.ToVector2() * EngineStatics.Aligner(anchor)).ToPoint();
+        _alignment = alignment;
+        actionBox.Location -= (actionBox.Size.ToVector2() * EngineStatics.Aligner(alignment)).ToPoint();
+        _actionBox = actionBox;
         
         if (hasHover)
-            HoverDetector = new HoverDetector(game, actionBox, Alignment.TopLeft);
+            HoverDetector = new HoverDetector(game, actionBox, alignment){Enabled = Enabled};
         
-        ActionBox = new Rectangle(
-            (int)(actionBox.X * EngineStatics.PartialScale),
-            (int)(actionBox.Y * EngineStatics.PartialScale),
-            (int)(actionBox.Width * EngineStatics.PartialScale),
-            (int)(actionBox.Height * EngineStatics.PartialScale)
-        );
         Image = texture;
-        Enabled = enabled;
         Visible = texture is not null;
         if (texture is not null)
             DrawOrder = texture.DrawOrder;
 
-        //ResetRectangle(null, null);
-        //Configs.ResolutionChanged += ResetRectangle;
+        UpdateActionBox(this, EventArgs.Empty);
+        EngineStatics.ViewportChanged += UpdateActionBox;
         Input.ButtonDown += Check;
-        
     }
 
     #region Methods
-
     public override void Draw(GameTime gameTime) 
     {
-        if (Image.Visible)
-            Image.Draw(gameTime);
+        if (Image.Visible) Image.Draw(gameTime);
     }
 
     private void Check(object s, ButtonEventArgs e)
     {
-        if (!Enabled || !ActionBox.Contains(e.Position))
+        if (!Enabled || !_actionBox.Contains(e.Position))
             return;
         
         switch (e.Button)
@@ -85,8 +101,14 @@ public class Button : DrawableGameComponent
         base.Update(gameTime);
     }
 
-    //private void ResetRectangle(object s, EventArgs e) => rectangle = new Rectangle((int)_position.X * Configs.Scale + Configs.XOffset, (int)_position.Y * Configs.Scale + Configs.YOffset, _size.X * Configs.Scale, _size.Y * Configs.Scale);
-
+    private void UpdateActionBox(object s, EventArgs e)
+    {
+        _actionBox = new Rectangle(
+            (_actionBox.Size.ToVector2() * EngineStatics.Scale + EngineStatics.Offset).ToPoint(),
+            (_actionBox.Size.ToVector2() * EngineStatics.Scale).ToPoint()
+        );
+    }
+    
     public new void Dispose()
     {
         //Configs.ResolutionChanged -= ResetRectangle;
--- MmgEngine/HoverDetector.cs
+++ MmgEngine/HoverDetector.cs
@@ -7,28 +7,52 @@ public class HoverDetector : GameComponent
 {
     public event EventHandler Hovered;
     public event EventHandler Unhovered;
-    public Rectangle ActionBox { get; private set; }
-    public bool Hovering { get; private set; }
 
-    public HoverDetector(Game game, Rectangle actionBox, Alignment alignment = Alignment.TopLeft, bool enabled = true) : base(game)
+    public Vector2 Position
     {
-        actionBox.Location -= (actionBox.Size.ToVector2() * EngineStatics.Aligner(alignment)).ToPoint();
-        
-        ActionBox = new Rectangle(
-            (int)(actionBox.X * EngineStatics.PartialScale),
-            (int)(actionBox.Y * EngineStatics.PartialScale),
-            (int)(actionBox.Width * EngineStatics.PartialScale),
-            (int)(actionBox.Height * EngineStatics.PartialScale)
-        );
-        Enabled = enabled;
+        get => new(_actionBox.X, _actionBox.Y);
+        set => _actionBox.Location = (value - _actionBox.Size.ToVector2() * EngineStatics.Aligner(_alignment)).ToPoint();
     }
 
+    public Vector2 Size
+    {
+        get => new(_actionBox.Width, _actionBox.Height);
+        set
+        {
+            _actionBox.Size = value.ToPoint();
+            _actionBox.Location -= (_actionBox.Size.ToVector2() * EngineStatics.Aligner(_alignment)).ToPoint();
+        }
+    }
+    
+    private Rectangle _actionBox;
+    private readonly Alignment _alignment;
+    public bool Hovering { get; private set; }
+
+    public HoverDetector(Game game, Rectangle actionBox, Alignment alignment = Alignment.TopLeft) : base(game)
+    {
+        _alignment = alignment;
+        actionBox.Location -= (actionBox.Size.ToVector2() * EngineStatics.Aligner(alignment)).ToPoint();
+        _actionBox = actionBox;
+        
+        UpdateActionBox(this, EventArgs.Empty);
+        
+        EngineStatics.ViewportChanged += UpdateActionBox;
+    }
+
+    private void UpdateActionBox(object s, EventArgs e)
+    {
+        _actionBox = new Rectangle(
+            (_actionBox.Size.ToVector2() * EngineStatics.Scale + EngineStatics.Offset).ToPoint(),
+            (_actionBox.Size.ToVector2() * EngineStatics.Scale).ToPoint()
+        );
+    }
+    
     public override void Update(GameTime gameTime)
     {
         if (!Game.IsActive)
             return;
 
-        if (ActionBox.Contains(Input.MousePoint))
+        if (_actionBox.Contains(Input.MousePoint))
         {
             if (Hovering) return;
             Hovering = true;
