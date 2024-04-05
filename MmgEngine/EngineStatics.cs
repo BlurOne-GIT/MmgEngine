879434c88ee9e04edfd648b46d174f05d119d966
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-04-05 18:29:07 -0300

    feat(EngineStatics): Viewport

--- MmgEngine/EngineStatics.cs
+++ MmgEngine/EngineStatics.cs
@@ -5,7 +5,27 @@ namespace MmgEngine;
 
 public static class EngineStatics
 {
-    public static float PartialScale { get; set; } = 1f;
+    public static event EventHandler ViewportChanged;
+    private static Vector2 _scale = Vector2.One;
+    private static Vector2 _offset = Vector2.Zero;
+    public static Vector2 Scale
+    {
+        get => _scale;
+        set
+        {
+            _scale = value;
+            ViewportChanged?.Invoke(_scale, EventArgs.Empty);
+        }
+    }
+    public static Vector2 Offset
+    {
+        get => _offset;
+        set
+        {
+            _offset = value;
+            ViewportChanged?.Invoke(_offset, EventArgs.Empty);
+        }
+    }
     public static bool WindowFocused { get; private set; }
     public static void Focus(object s, EventArgs e) => WindowFocused = true;
     public static void UnFocus(object s, EventArgs e) => WindowFocused = false;
