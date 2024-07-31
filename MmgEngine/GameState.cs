5092cee8620ecf97843fe8046841032eb4104986
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-07-31 18:14:55 -0300

    feat(GameState): state enabled and visible
    The only downside of this system is that components' Enabled and Visible may still be flipped to true while the GameState's is false.

--- MmgEngine/GameState.cs
+++ MmgEngine/GameState.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
 namespace MmgEngine;
@@ -43,12 +45,73 @@ public abstract class GameState : DrawableGameComponent
     /// <param name="gameState">New game state.</param>
     protected void SwitchState(GameState gameState) => OnStateSwitched?.Invoke(this, gameState);
 
-    private void OnComponentAdded(object s, GameComponentCollectionEventArgs e) => Game.Components.Add(e.GameComponent);
+    private void OnComponentAdded(object s, GameComponentCollectionEventArgs e)
+    {
+        if (!Enabled && e.GameComponent is GameComponent { Enabled: true } gameComponent)
+        {
+            _previousEnabled.Add(gameComponent);
+            gameComponent.Enabled = false;
+        }
+
+        if (!Visible && e.GameComponent is DrawableGameComponent { Visible: true } drawable)
+        {
+            _previousVisible.Add(drawable);
+            drawable.Visible = false;
+        }
+        
+        Game.Components.Add(e.GameComponent);
+    }
 
     private void OnComponentRemoved(object s, GameComponentCollectionEventArgs e)
     {
         Game.Components.Remove(e.GameComponent);
         if (e.GameComponent is IDisposable disposable)
             disposable.Dispose();
+    }
+    
+    private readonly HashSet<GameComponent> _previousEnabled = new();
+    private readonly HashSet<DrawableGameComponent> _previousVisible = new();
+    
+    protected override void OnEnabledChanged(object sender, EventArgs args)
+    {
+        if (Enabled)
+        {
+            foreach (var component in Components)
+                if (component is GameComponent gameComponent && _previousEnabled.Remove(gameComponent))
+                    gameComponent.Enabled = true;
+        }
+        else
+        {
+            foreach (var component in Components)
+                if (component is GameComponent { Enabled: true } gameComponent)
+                {
+                    _previousEnabled.Add(gameComponent);
+                    gameComponent.Enabled = false;
+                }
+        }
+        
+        base.OnEnabledChanged(sender, args);
+    }
+
+    protected override void OnVisibleChanged(object sender, EventArgs args)
+    {
+        if (Visible)
+        {
+            foreach (var component in Components)
+                if (component is DrawableGameComponent drawable && _previousVisible.Remove(drawable))
+                    drawable.Visible = true;
+        }
+        else
+        {
+            foreach (var component in Components)
+                if (component is DrawableGameComponent { Visible: true } drawable)
+                {
+                    _previousVisible.Add(drawable);
+                    drawable.Visible = false;
+                }
+        }
+        
+        
+        base.OnVisibleChanged(sender, args);
     }
 }
